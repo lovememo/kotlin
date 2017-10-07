48b533188f13a720c30208373235b5a31d7034bc
lovememo <lovememo@foxmail.com>
2017-10-07 22:25:50 +0800

    PackageDemoTest

---
+++ src/main/kotlin/Test.kt
@@ -0,0 +1,8 @@
+/**
+ * Created by lovememo on 17-10-7.
+ */
+
+fun main(args: Array<String>) {
+    val name = "Peter"
+    println("Hello, my name is $name")
+}
---
+++ src/main/kotlin/com/easy/kotlin/PackageDemo.kt
@@ -0,0 +1,18 @@
+package com.easy.kotlin
+
+/**
+ * Created by lovememo on 17-10-7.
+ */
+fun what(){
+    println("This is WHAT ?")
+}
+
+class Motorbike{
+    fun drive(){
+        println("Drive The Motorbike ...")
+    }
+}
+
+fun main(args:Array<String>){
+    println("Hello,World!")
+}
